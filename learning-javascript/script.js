af23fd110a618247a0b4716ef5c78f58fbf6e510
fmgb2025 <116223692+fmgb2025@users.noreply.github.com>
2024-11-21 10:39:38 -0600

    safe

---
+++ learning-javascript/script.js
@@ -0,0 +1,16 @@
+//const myName = "Ms Ifft";
+//let age = 67;
+//console.log(myName);
+//console.log(age);
+//
+//console.log("Hi my name is", myName, "and my age is", age + 1);
+//
+//console.log("The Sum of 67+67 is", age + age);
+//
+//console.log(age + age + "is the sum of 67+67");
+
+function reveal() {
+    const mySpan = document.getElementById("name");
+    mySpan.innerText = "Frances";
+    //alert("This works!");
+}
