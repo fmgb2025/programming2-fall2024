64ad5dbd83b7f35c0e00595bede4982fd829bbde
fmgb2025 <116223692+fmgb2025@users.noreply.github.com>
2024-12-10 12:25:22 -0600

    hw

---
+++ to-do-list/script.js
@@ -0,0 +1,59 @@
+let tasks = ["Fold Laudry", "Make Dinner"];
+
+function showTasks() {
+    let taskList = document.getElementById("task-list");
+    taskList.innerHTML = "";
+    for (i = 0; i < tasks.length; i++) {
+        let li = document.createElement("li");
+        li.innerText = tasks[i];
+        taskList.appendChild(li);
+    }
+}
+
+function addTasks() {
+    let task = prompt("Enter your task: ");
+    tasks.push(task);
+    showTasks();
+}
+
+function rmTasks() {
+    let taskFound = false;
+    let task = prompt("Enter task to remove: ");
+    task = task.toLowerCase();
+
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].toLowerCase() === task) {
+            tasks.splice(i, 1);
+            taskFound = true;
+        }
+    }
+    if (!taskFound) {
+        alert("task not found");
+    }
+
+    showTasks();
+}
+
+function clearTasks() {
+    tasks = [];
+    showTasks();
+}
+
+function prioritizeTasks() {
+    let task = prompt("Enter task to prioritize: ");
+    task = task.toLowerCase();
+    let taskFound = false;
+
+    let taskList = document.getElementById("task-list");
+    let listItems = taskList.getElementsByTagName("li");
+
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].toLowerCase() === task) {
+            listItems[i].style.color = "yellow";
+            taskFound = true;
+        }
+    }
+    if (!taskFound) {
+        alert("task not found");
+    }
+}
